1273248f24dd52ad35d632c9ceb4f61376a50174
Lauren Seward <lauren.unal@gmail.com>
2024-02-28 16:27:40 -0600

    update component architecture

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello, welcome to Nourish Nest!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
