439f890f88611ac97ebc1e94385ca5521f6aa2b7
Lauren Seward <lauren.unal@gmail.com>
2024-02-29 09:49:23 -0600

    Update Header component to NavBar

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom';
 import "./App.css";
 import { getFoodData } from "../../api/apiCalls";
 
@@ -8,7 +9,7 @@ const App = () => {
     .then(data => console.log(data))
     .catch(error => console.error(error))
   }, []);
-  
+
   return (
     <div className="App">
       <header className="App-header">
